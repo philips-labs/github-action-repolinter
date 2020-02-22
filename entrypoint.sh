3531e5b25cc489e4c8807bb39dcee9f9c61d05c9
Jeroen Knoops <jeroen.knoops@gmail.com>
2020-02-22 08:06:18 +0100

    error in entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ npm install -g log-symbols
 npm install -g todogroup/repolinter 
 
 echo "[INFO] Checking custom repolint file"
-if [ -n "$CUSTOM_REPOLINT_FILE ]; then
+if [ -n "$CUSTOM_REPOLINT_FILE" ]; then
   echo "[INFO] Using custom repolint file"
   cp $CUSTOM_REPOLINT_FILE /repolinter/ 
 fi
