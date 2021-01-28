2434659e2d8a8a550937f874d6bc7ccfb74b4c0f
Danno Ferrin <danno.ferrin@gmail.com>
2021-01-28 13:41:15 -0700

    Add support for rulesetUrl
    Signed-off-by: Danno Ferrin <danno.ferrin@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,26 @@
 #!/bin/sh
 
+echo "::group::Setup Repolinter"
 set -euo
 
 echo "[INFO] Installing todogroup/repolinter"
 npm install -g log-symbols 
 npm install -g todogroup/repolinter 
 
-if [ -z "$CUSTOM_REPOLINT_FILE" ]; then
+if [ -n "$CUSTOM_REPOLINT_URL" ]; then
+  echo "[INFO] Insert CUSTOM configuration url"
+  REPOLINT_ARG="--rulesetUrl $CUSTOM_REPOLINT_URL"
+elif [ -n "$CUSTOM_REPOLINT_FILE" ]; then
+  echo "[INFO] Insert CUSTOM configuration file"
+  REPOLINT_ARG="--rulesetFile $CUSTOM_REPOLINT_FILE"
+else
   echo "[INFO] Insert default configuration"
   cp /repolinter/repolint.yml .
-  REPOLINT_FILE=repolint.yml
-else
-  echo "[INFO] Insert CUSTOM configuration"
-  REPOLINT_FILE=$CUSTOM_REPOLINT_FILE
+  REPOLINT_ARG="--rulesetFile repolint.yml"
 fi
 
 echo "[INFO] Executing:"
 echo "[INFO] repolinter $*"
-sh -c "repolinter --rulesetFile $REPOLINT_FILE $*"
+echo "::endgroup::"
+
+sh -c "repolinter $REPOLINT_ARG $*"
